cbf2a42d338baa48cc811c75f2129f512dcd7f7f
iani and chris funk <zannos@gmail.com>
2017-05-25 17:28:10 +0000

    selected essensial files

---
+++ 03startpython1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# /usr/local/bin/jackd -P75 -dalsa -dhw:1 -p1024 -n3 -s -r44100 &
+# su root -c "sclang -D /home/pi/sc.scd"
+sleep 10
+sudo python /home/pi/Adafruit_Python_MPR121/examples/basic.py &
+# sleep 1
+# sudo python /home/pi/Adafruit_Python_MPR121/examples/basic2.py &
+
